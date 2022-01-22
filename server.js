04133b6344d9257ac0ed364aa95cca1093e531af
akhilpamwani <83491855+akhilpamwani@users.noreply.github.com>
2022-01-22 20:27:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 // Mongodb Connection 
 
   
-mongoose.connect('mongodb://127.0.0.1:27017/Contact',{
+mongoose.connect(process.env.DATABASE,{
   useNewUrlParser: true,
  
   useUnifiedTopology: true,
@@ -43,7 +43,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/Contact',{
 app.use(express.json());
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://aryanpamwani.netlify.app","http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
