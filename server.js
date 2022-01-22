69f9e4137734127be68a9fbb584db77d5f4cc4e7
akhilpamwani <83491855+akhilpamwani@users.noreply.github.com>
2022-01-22 20:23:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,6 +42,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/Contact',{
 
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 // Deploying Website 
